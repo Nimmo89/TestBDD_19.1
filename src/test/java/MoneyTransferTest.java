9db18130edbf26aabe01ea18e756335efa5b8164
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-10-24 19:17:32 +0700

    Two test

--- src/test/java/MoneyTransferTest.java
+++ src/test/java/MoneyTransferTest.java
@@ -8,18 +8,34 @@ import static com.codeborne.selenide.Selenide.*;
 public class MoneyTransferTest {
 
     @Test
-    void shouldTransferMoneyBetweenOwnCards() {
+    void shouldTransferWithOneOnTwo() {
         open("http://localhost:9999");
         var loginPage = new LoginPage();
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
         verificationPage.validVerify(verificationCode);
-        $("[data-test-id=action-deposit]").click();
+        $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] .button").click();
         $("[data-test-id=amount] input").setValue("500");
         $("[data-test-id=from] input").setValue("5559000000000002");
         $("[data-test-id=action-transfer]").click();
         $("[data-test-id=dashboard]").shouldBe(visible);
-        $("[data-test-id=92df3f1c-a033-48e6-8390-206f6b1f56c0]").shouldHave();
+        $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']").shouldHave();
+    }
+
+    @Test
+    void shouldTransferWithTwoOnOne() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+        $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] .button").click();
+        $("[data-test-id=amount] input").setValue("500");
+        $("[data-test-id=from] input").setValue("5559000000000001");
+        $("[data-test-id=action-transfer]").click();
+        $("[data-test-id=dashboard]").shouldBe(visible);
+        $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']").shouldHave();
     }
 }
