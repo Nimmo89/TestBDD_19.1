e145800954418e6b2e54347a650d0aa2857c99ff
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-10-26 21:33:46 +0700

    Исправления и доработки

--- src/test/java/MoneyTransferTest.java
+++ src/test/java/MoneyTransferTest.java
@@ -1,13 +1,12 @@
 import org.junit.jupiter.api.Test;
 import data.DataHelper;
-import page.DashboardPage;
+//import page.DashboardPage;
 import page.LoginPage;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MoneyTransferTest {
-    public DashboardPage dashboardPage = new DashboardPage();
 
     @Test
     void shouldTransferWithOneOnTwo() {
@@ -16,13 +15,15 @@ public class MoneyTransferTest {
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+        var cardBalance = dashboardPage.getOneCardBalance();
         verificationPage.validVerify(verificationCode);
         $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] .button").click();
         $("[data-test-id=amount] input").setValue("500");
         $("[data-test-id=from] input").setValue("5559000000000002");
         $("[data-test-id=action-transfer]").click();
         $("[data-test-id=dashboard]").shouldBe(visible);
-        $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']").shouldHave();
+        $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']").shouldHave(text(String.valueOf(cardBalance)));
     }
 
     @Test
@@ -32,6 +33,8 @@ public class MoneyTransferTest {
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+        var cardBalance = dashboardPage.getTwoCardBalance();
         verificationPage.validVerify(verificationCode);
         $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] .button").click();
         $("[data-test-id=amount] input").setValue("500");
--- src/test/java/page/DashboardPage.java
+++ src/test/java/page/DashboardPage.java
@@ -13,23 +13,29 @@ public class DashboardPage {
         heading.shouldBe(visible);
     }
 
-//private ElementsCollection cards = $$(".list__item");
-//    private final String balanceStart = "баланс: ";
-//    private final String balanceFinish = " р.";
+private ElementsCollection cards = $$(".list__item");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
 
 //    public Dashboard() {
 //    }
 
-//    public int getCardBalance() {
-//        val text = $("[data-test-id=92df3f1c-a033-48e6-8390-206f6b1f56c0]");
-//        val text2 = $("[data-test-id=0f3f5c2a-249e-4c3d-8287-09f7a039391d]");
-//        return extractBalance(text);
-//    }
+    public int getOneCardBalance() {
+        val text = cards.first().text();
+//        val oneCard = cards.get(1).$("[data-test-id=92df3f1c-a033-48e6-8390-206f6b1f56c0]");
+        return extractBalance(text);
+    }
 
-//    public int extractBalance(String text) {
-//        val start = text.indexOf(balanceStart);
-//        val finish = text.indexOf(balanceFinish);
-//        val value = text.substring(start + balanceStart.length() + finish);
-//        return Integer.parseInt(value);
-//    }
+    public int getTwoCardBalance() {
+        val text = cards.last().text();
+//        val twoCard = cards.get(2).$("[data-test-id=0f3f5c2a-249e-4c3d-8287-09f7a039391d]")
+        return extractBalance(text);
+    }
+
+    public int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length() + finish);
+        return Integer.parseInt(value);
+    }
 }
